ecce68e2836d9ba21513b11d471170ee170c8eba
ThienNguyen2298 <thiennguyen.it.ute@gmail.com>
2019-07-03 08:58:29 +0700

    init

---
+++ HMS.Entities/Booking.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelManagement.Entities
+{
+    public class Booking
+    {
+        public int ID { get; set; }
+        public int AccomodationID { get; set; }
+        public Accomodation Accomodation { get; set; }
+        public DateTime dateTime { get; set; }
+        public int Duration { get; set; }
+    }
+}
